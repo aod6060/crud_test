3f7215f36b7b538f7aca5b1931cbeba951790b81
derf6060 <derf6060@derfnet.attlocal.net>
2024-04-26 12:53:07 -0500

    Started working on configuration for my little template.

--- backend.js
+++ backend.js
@@ -1,3 +1,8 @@
 /*
     This is the applications starting point for every project.
 */
+
+const conf = require('./config')
+
+conf.init();
+conf.run();
--- config.js
+++ config.js
@@ -1,3 +1,30 @@
 /*
-    
+    This is were all of nodes configuration will be placed in
 */ 
+module.exports = {
+    init: () => {
+        this.express = require('express')
+        this.app = this.express()
+        this.port = 3000;
+        this.bodyParser = require('body-parser')
+        this.app.use(this.express.static('public'));
+        this.app.use(this.bodyParser.json());
+        this.app.use(this.bodyParser.urlencoded({extended: true}));
+        this.app.set('view engine', 'ejs');
+
+        // Init Models
+
+        // Init Controller
+        // I'll add a tempory rout for testing.
+        this.app.get('/', (req, res) => {
+            res.send("Hello, World");
+        });
+
+        this.listen_log = () => {
+            console.log(`Application running on ${this.port}`)
+        };
+    },
+    run: () => {
+        this.app.listen(this.port, this.listen_log)
+    }
+};
